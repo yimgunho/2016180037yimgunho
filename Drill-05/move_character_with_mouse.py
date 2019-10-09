5eb6fb41457b3223e2692be89b68cf17d2c53b19
yimgunho-notebook <rjsgh2119@gmail.com>
2019-10-09 14:30:39 +0900

    2016180037 임건호 Drill-05 커밋 횟수 : 023
    마우스 커서 검지손가락으로 구현

--- Drill-05/move_character_with_mouse.py
+++ Drill-05/move_character_with_mouse.py
@@ -1,17 +1,19 @@
 from pico2d import *
 
 KPU_WIDTH, KPU_HEIGHT = 1280, 824
-
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
+cursor = load_image('hand_arrow.png')
+hide_cursor()
 
 play = True
 frame = 0
+x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 
 
 def handle_events():
-    global play
+    global play, x, y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -19,12 +21,16 @@ def handle_events():
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_ESCAPE:
                 play = False
+        elif event.type == SDL_MOUSEMOTION:
+            x, y = event.x, KPU_HEIGHT - 1 - event.y
 
 
 while play:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     character.clip_draw(frame * 100, 100 * 1, 100, 100, 500, 500)
+    cursor.draw(x + 25, y + 26)
+
     update_canvas()
 
     handle_events()
