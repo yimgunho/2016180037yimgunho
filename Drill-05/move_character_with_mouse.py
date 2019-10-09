da8c84243a4bb309bebc291396054c528dd21b4b
yimgunho-notebook <rjsgh2119@gmail.com>
2019-10-09 14:48:27 +0900

    2016180037 임건호 Drill-05 커밋 횟수 : 06
    클릭시 가만히 있는 애니메이션 구현

--- Drill-05/move_character_with_mouse.py
+++ Drill-05/move_character_with_mouse.py
@@ -9,14 +9,15 @@ hide_cursor()
 
 play = True
 frame = 0
-x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+now_x, now_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+default_x, default_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 dir_x = 0
 mouse_button = 0
 stop = 1
 
 
 def handle_events():
-    global play, x, y, dir_x, mouse_button
+    global play, default_x, default_y, now_x, now_y, dir_x, mouse_button
 
     events = get_events()
     for event in events:
@@ -26,13 +27,17 @@ def handle_events():
             if event.key == SDLK_ESCAPE:
                 play = False
         elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, KPU_HEIGHT - 1 - event.y
+            now_x, now_y = event.x, KPU_HEIGHT - 1 - event.y
+
         elif event.type == SDL_MOUSEBUTTONDOWN:
             mouse_button += 1
-            if x > event.x:
+            if default_x < event.x:
                 dir_x += 1;
-            elif x < event.x:
-                dir_x += 1;
+            elif default_x > event.x:
+                dir_x -= 1;
+            default_x = now_x
+            default_y = now_y
+
         elif event.type == SDL_MOUSEBUTTONUP:
             mouse_button -= 1
 
@@ -40,9 +45,14 @@ def handle_events():
 while play:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
+    if dir_x >= 0:
+        character.clip_draw(100, 100 * 3, 100, 100, default_x, default_y)
+    elif dir_x < 0:
+        character.clip_draw(100, 100 * 2, 100, 100, default_x, default_y)
+
     if mouse_button > 0:
-        character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
-    cursor.draw(x + 25, y - 26)
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, now_x, now_y)
+    cursor.draw(now_x + 25, now_y - 26)
 
     update_canvas()
 
